bb25934edfdd4e4ad47a27e473019054f9c83929
codeabuu <abdulngui718@gmail.com>
2024-03-09 20:59:04 +0000

    first API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,4 +1,4 @@
-/* eslint-disable import/no-named-as-default */
+// eslint-disable-next-line import/no-named-as-default
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
@@ -10,10 +10,16 @@ export default class AppController {
     });
   }
 
-  static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      });
+  static async getStats(req, res) {
+    try {
+      const [usersCount, filesCount] = await Promise.all([
+        dbClient.nbUsers(),
+        dbClient.nbFiles()
+      ]);
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (error) {
+      console.error("Error fetching stats:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
   }
 }
